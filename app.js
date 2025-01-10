fcc8bc528ff1583ced3e7684ae744eee1d5e21a3
Josue Marchena <josuemarchena0730@gmail.com>
2025-01-09 21:52:53 -0500

    added styles section home, about🚀

---
+++ app.js
@@ -0,0 +1,28 @@
+// // Selecciona los elementos principales
+// const customSelect = document.querySelector(".custom-select");
+// const selectButton = customSelect.querySelector(".select-button");
+// const options = customSelect.querySelectorAll(".option");
+
+// // Abrir y cerrar el menú
+// customSelect.addEventListener("click", (e) => {
+//     // Previene que el evento cierre el menú inmediatamente después de abrir
+//     e.stopPropagation();
+//     customSelect.classList.toggle("active");
+// });
+
+// // Seleccionar una opción
+// options.forEach((option) => {
+//     option.addEventListener("click", (e) => {
+//         // Actualiza el texto del botón
+//         selectButton.textContent = option.textContent;
+//         // Cierra el menú
+//         customSelect.classList.remove("active");
+//         // Previene la propagación para no cerrar accidentalmente al seleccionar
+//         e.stopPropagation();
+//     });
+// });
+
+// // Cerrar el menú al hacer clic fuera del contenedor
+// document.addEventListener("click", () => {
+//     customSelect.classList.remove("active");
+// });
